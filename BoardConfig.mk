07188bbe49b08472e2c0eb58fb0b98c88edf4d94
ShivamKumarJha <jha.shivam3@gmail.com>
2018-10-27 05:06:04 +0530

    X00TD: OTA 334 sync
    Change-Id: I750d37e3208e0ba19cd0d9df57b90b9101904fdd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -257,7 +257,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-09-01
+VENDOR_SECURITY_PATCH := 2018-10-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
